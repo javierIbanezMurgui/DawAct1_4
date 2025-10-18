da9df88a41ffabfe7539a1cba7c3695cce3dd82b
javier ibanez <javi@gmail.com>
2025-10-18 22:25:04 +0200

    corregdo el error de estar index.html dentro e carpeta oculta

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 function mostrarMensaje() {
-    document.getElementById("mensaje").textContent = "Prueba java script";
+    document.getElementById("mensaje").innerHTML = 
+        '<a href="https://javieribanezmurgui.github.io/DawAct1_4/" target="_blank">Ir a mi repositorio Markdown</a>';
 }
